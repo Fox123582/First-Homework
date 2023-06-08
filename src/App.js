6bfb90b3ffe9bcfe217b09344ce010f65a901bbd
Fox123582 <79007252+Fox123582@users.noreply.github.com>
2023-06-09 02:29:08 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,28 +3,47 @@ import {Route, Routes} from "react-router-dom";
 import LogIn from "./components/loginPage/logIn";
 import preventStyles from './common/commonStyles/preventstyles.css'
 import MainPage from "./components/mainPage/mainPage";
-import AppContext from "./context/appContext";
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {setPerson} from "./store/userSlice";
 import local from './services/localStorage'
+import MainPageMUI from "./components/mainPageWithMUI/mainPageMUI";
+import PrivateRouteForRegisterPage from "./hoc/PrivateRouteForRegisterPage";
+import ShoppingCartPage from "./components/shoppingCartPage/shoppingCartPage";
+import PrivateRouteForCart from "./hoc/privateRouteForCart";
 
 function App() {
 
+    const [isAuth,IsAuthSet] = useState(false)
     const dispatch = useDispatch()
     const person = useSelector(state=>state.userSlice)
 
     useEffect(()=>{
+        IsAuthSet(local.getFromLocalstorage().id ? true : false)
         dispatch(setPerson(local.getFromLocalstorage()))
     },[])
+
+    useEffect(()=>{
+        IsAuthSet(local.getFromLocalstorage().id ? true : false)
+    },[person])
 
   return (
     <div className="App">
 
         <Header person = {person}></Header>
         <Routes>
-            <Route path={'/login'} element={<LogIn/>}/>
-            <Route path={''} element={<MainPage/>}/>
+            <Route path={'/login'} element={
+                <PrivateRouteForRegisterPage isAuth={isAuth}>
+                    <LogIn/>
+                </PrivateRouteForRegisterPage>
+            }/>
+            <Route path={''} element={<MainPageMUI auth={isAuth}/>}/>
+            {/*<Route path={''} element={<MainPage/>}/>*/}
+            <Route path={'/shoppingCart'} element={
+                <PrivateRouteForCart isAuth={isAuth}>
+                    <ShoppingCartPage></ShoppingCartPage>
+                </PrivateRouteForCart>
+            }/>
         </Routes>
     </div>
   );
